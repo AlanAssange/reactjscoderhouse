e85928950d4d604f44baba35e47a7fd356b4b5e2
Alan Cañellas <alanjc27@gmail.com>
2021-11-29 16:55:27 -0300

    cambios en el router 2

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Navigate, useRoutes } from 'react-router'
 import { CartView } from '../components/CartView'
 import { ItemDetailContainer } from '../components/ItemDetailContainer'
+import { ItemList } from '../components/ItemList'
 import { ItemListContainer } from '../components/ItemListContainer'
 
 export const AppRouter = () => {
 
     const routes = useRoutes([
 
-        {path: "/", element: <ItemListContainer/>},
+        {path: "/", element: <ItemList/>},
         {path: "/productos/:catId", element: <ItemListContainer/>},
         {path: "/detail/:itemId", element: <ItemDetailContainer/>},
         {path: "/cart", element: <CartView/>},
@@ -17,9 +18,5 @@ export const AppRouter = () => {
     ])
 
 
-    return (
-        <div>
-            
-        </div>
-    )
+    return routes
 }
